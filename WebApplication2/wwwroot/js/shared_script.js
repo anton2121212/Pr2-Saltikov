ddc0d0902934bea4de1f0cbf034e7d0a291b63f9
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2024-11-05 16:46:29 +0200

    Add files via upload

---
+++ WebApplication2/wwwroot/js/shared_script.js
@@ -0,0 +1,18 @@
+﻿
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        var header = document.getElementById('header-top');
+        var scrollTop = window.scrollY;
+        var maxScroll = 250;
+
+        var opacity = Match.min(scrollTop / maxScroll, 1);
+        header.style.backgroundColor = 'rgba(255, 165, 0, ${opacity})';
+    });
+});
+
+.container {
+    width: 82 %;
+    border: 2px solid #cacaca;
+    border - radius: 3px;
+    box - shadow: 5px 5px 10px rgba(0, 0, 0, 0.4);
+}
